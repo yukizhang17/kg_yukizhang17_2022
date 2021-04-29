0bf6716ef7c1f7dcb010da4c1ef891b4ad1c9cef
yukizhang17 <zhangy3@whitman.edu>
2021-04-29 17:39:20 -0400

    Set up a conversion object to convert each number from 0-9 to phonetic version

---
+++ main.js
@@ -0,0 +1,8 @@
+/* 
+This is a program that convert an array of integers into an array 
+of strings representing the phonetic equivalent of the integer.
+*/
+
+/* Give a global var which will do the conversion */
+var num_to_str = {"0":"Zero", "1":"One", "2":"Two", "3":"Three", "4":"Four", "5":"Five", 
+                  "6":"Six", "7":"Seven", "8":"Eight", "9":"Nine"}
