cd09eb6c64a0aa4d44c478525f7859a5e2130755
yukizhang17 <zhangy3@whitman.edu>
2021-04-29 17:41:09 -0400

    Extract the input integer value from argv

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ of strings representing the phonetic equivalent of the integer.
 /* Give a global var which will do the conversion */
 var num_to_str = {"0":"Zero", "1":"One", "2":"Two", "3":"Three", "4":"Four", "5":"Five", 
                   "6":"Six", "7":"Seven", "8":"Eight", "9":"Nine"}
+
+/* Get the input array from argv and extract them out */
+var argv = process.argv;
+var input = argv.slice(2);
+console.log(input);
